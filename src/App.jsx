0995d63751e7ead700935e5170e2e974bd310666
FaaizAhmad6420 <faizahmad6420@gmail.com>
2025-01-29 15:36:54 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold p-4 bg-orange-500 text-white rounded-lg '>Hello World!</h1>
+      <h1>abc</h1>
+      <h6>abc</h6>
+    </>
+  )
+}
+
+export default App
